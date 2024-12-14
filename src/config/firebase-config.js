546c5d073d3532e2b0983b1e6b01b5fd917c7529
mahak porwal <porwalmahak784@gmail.com>
2024-12-14 16:14:23 +0530

    done

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,26 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-//import { getAuth,GoogleAuthProvider } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-//import { getAuth } from 'firebase/auth';
-import { getAuth, GoogleAuthProvider,signInWithPopup} from 'firebase/auth';
-// Your web app's Firebase configuration
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBzFO94n6a678qItHoqfAjviqwB8A9mU-8",
-  authDomain: "money-20159.firebaseapp.com",
-  projectId: "money-20159",
-  storageBucket: "money-20159.firebasestorage.app",
-  messagingSenderId: "83596875880",
-  appId: "1:83596875880:web:2a6270acbce1dbce02c3dd"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
 export const provider = new GoogleAuthProvider();
-
-export const db=getFirestore(app)
+export const db = getFirestore(app);
